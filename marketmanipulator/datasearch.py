f61f22fbf90aa8409318af23cde4baf06063168f
Troy Squillaci <Zivia@Troys-MacBook-Pro.local>
2015-06-09 21:54:04 -0600

    Added User Defined Exception - InvalidIdError
    Currently located (and used in) datasearch.py.

--- marketmanipulator/datasearch.py
+++ marketmanipulator/datasearch.py
@@ -6,37 +6,50 @@ import datautils as du
 from texttable import Texttable
 
 
+class InvalidIdError(Exception):
+
+    def __init__(self, value):
+
+        self.value = value
+
+    def __str__(self):
+
+        return repr(self.value)
+
+
 def search_commerce_listings(ids=None):
+
+    if not (isinstance(ids, list) and all(ids[x] > 0 for x in xrange(len(ids)))):
+
+        raise InvalidIdError('input must be list of positive integers')
 
     buy_results = list()
     buy_results.append(['Buy Listings', 'Buy Unit Price', 'Buy Quantity'])
     sell_results = list()
     sell_results.append(['Sell Listings', 'Sell Unit Price', 'Sell Quantity'])
 
-    # Fetch commerce listings data.
     commerce_listings = tp.commerce_listings(ids)
 
     for commerce_listing in commerce_listings:
 
         print 'Commerce Listing Data for ' + it.items(commerce_listing[u'id'])[0][u'name'] + '\n'
 
-        for x in xrange(len(commerce_listing[u'buys'])):
+        for buy in xrange(len(commerce_listing[u'buys'])):
 
-            buy_listings = commerce_listing[u'buys'][x][u'listings']
-            buy_unit_price = du.format_coin(commerce_listing[u'buys'][x][u'unit_price'])
-            buy_quantity = commerce_listing[u'buys'][x][u'quantity']
+            buy_listings = commerce_listing[u'buys'][buy][u'listings']
+            buy_unit_price = du.format_coin(commerce_listing[u'buys'][buy][u'unit_price'])
+            buy_quantity = commerce_listing[u'buys'][buy][u'quantity']
 
             buy_results.append([buy_listings, buy_unit_price, buy_quantity])
 
-        for x in xrange(len(commerce_listing[u'sells'])):
+        for sell in xrange(len(commerce_listing[u'sells'])):
 
-            sell_listings = commerce_listing[u'sells'][x][u'listings']
-            sell_unit_price = du.format_coin(commerce_listing[u'sells'][x][u'unit_price'])
-            sell_quantity = commerce_listing[u'sells'][x][u'quantity']
+            sell_listings = commerce_listing[u'sells'][sell][u'listings']
+            sell_unit_price = du.format_coin(commerce_listing[u'sells'][sell][u'unit_price'])
+            sell_quantity = commerce_listing[u'sells'][sell][u'quantity']
 
             sell_results.append([sell_listings, sell_unit_price, sell_quantity])
 
-        # Tables for pretty printing.
         buy_table = Texttable()
         buy_table.set_deco(Texttable.HEADER)
         buy_table.set_cols_dtype(['t', 't', 't'])
@@ -55,17 +68,19 @@ def search_commerce_listings(ids=None):
 
 def search_commerce_prices(ids=None):
 
+    if not (isinstance(ids, list) and all(ids[x] > 0 for x in xrange(len(ids)))):
+
+        raise InvalidIdError('input must be list of positive integers')
+
     search_results = list()
     search_results.append(['Item Name', 'Buy Price', 'Sell Price', 'Demand', 'Supply'])
 
     for id in ids:
 
-        # Fetch item data.
         item = it.items(id)
 
         item_name = item[0][u'name']
 
-        # Fetch commerce price data.
         commerce_price = tp.commerce_prices(id)
 
         buy_price = du.format_coin(commerce_price[0][u'buys'][u'unit_price'])
@@ -75,7 +90,6 @@ def search_commerce_prices(ids=None):
 
         search_results.append([item_name, buy_price, sell_price, demand, supply])
 
-    # Table for pretty printing.
     search_table = Texttable()
     search_table.set_deco(Texttable.HEADER)
     search_table.set_cols_dtype(['t', 't', 't', 't', 't'])
