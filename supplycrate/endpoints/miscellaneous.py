7743a4e650a4758ec29896198b26434ec21cbb32
Troy Squillaci <Zivia@Troys-MacBook-Pro.local>
2015-08-03 01:05:01 -0600

    Added Endpoint for 'Miscellaneous'
    See title.

---
+++ supplycrate/endpoints/miscellaneous.py
@@ -0,0 +1,93 @@
+__author__ = 'Zivia'
+
+import supplycrate.dataservice as ds
+
+
+def build():
+
+    return ds.pull_data('https://api.guildwars2.com/v2/build')
+
+
+def colors(ids=None):
+
+    if ids is None:
+
+        return ds.pull_data('https://api.guildwars2.com/v2/colors')
+
+    else:
+
+        if isinstance(ids, list):
+
+            return ds.pull_data('https://api.guildwars2.com/v2/colors?ids=' + ','.join(map(str, ids)))
+
+        else:
+
+            return ds.pull_data('https://api.guildwars2.com/v2/colors?ids=' + str(ids))
+
+
+def files(ids=None):
+
+    if ids is None:
+
+        return ds.pull_data('https://api.guildwars2.com/v2/files')
+
+    else:
+
+        if isinstance(ids, list):
+
+            return ds.pull_data('https://api.guildwars2.com/v2/files?ids=' + ','.join(map(str, ids)))
+
+        else:
+
+            return ds.pull_data('https://api.guildwars2.com/v2/files?ids=' + str(ids))
+
+
+def quaggans(ids=None):
+
+    if ids is None:
+
+        return ds.pull_data('https://api.guildwars2.com/v2/quaggans')
+
+    else:
+
+        if isinstance(ids, list):
+
+            return ds.pull_data('https://api.guildwars2.com/v2/quaggans?ids=' + ','.join(map(str, ids)))
+
+        else:
+
+            return ds.pull_data('https://api.guildwars2.com/v2/quaggans?ids=' + str(ids))
+
+
+def specializations(ids=None):
+
+    if ids is None:
+
+        return ds.pull_data('https://api.guildwars2.com/v2/specializations')
+
+    else:
+
+        if isinstance(ids, list):
+
+            return ds.pull_data('https://api.guildwars2.com/v2/specializations?ids=' + ','.join(map(str, ids)))
+
+        else:
+
+            return ds.pull_data('https://api.guildwars2.com/v2/specializations?ids=' + str(ids))
+
+
+def worlds(ids=None):
+
+    if ids is None:
+
+        return ds.pull_data('https://api.guildwars2.com/v2/worlds')
+
+    else:
+
+        if isinstance(ids, list):
+
+            return ds.pull_data('https://api.guildwars2.com/v2/worlds?ids=' + ','.join(map(str, ids)))
+
+        else:
+
+            return ds.pull_data('https://api.guildwars2.com/v2/worlds?ids=' + str(ids))
